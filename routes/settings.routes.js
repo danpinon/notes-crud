f6dc295a96db60b0875e8b96b88d448adb7fbed1
albertopformoso <albertopformoso@gmail.com>
2021-05-19 15:58:44 -0500

    Settings page almost complete

--- routes/settings.routes.js
+++ routes/settings.routes.js
@@ -8,40 +8,150 @@ const saltRounds = 10
 /* GET home page */
 router.route('/settings/:id')
   .get((req, res, next) => {
-  res.render('settings/settings', { userInSession: req.session.currentUser });
+    const { id } = req.params
+    User.findById(id)
+      .then((theUser) => {
+        res.render(
+          'settings/settings', 
+          {
+            userInSession: req.session.currentUser,
+            userData: theUser,
+            userDisplay: {
+              content: 'hidden',
+              changeButton: '',
+              cancelButton: 'hidden'
+            },
+            emailDisplay: {
+              content: 'hidden',
+              changeButton: '',
+              cancelButton: 'hidden'
+            },
+            passwordDisplay: {
+              content: 'hidden',
+              changeButton: '',
+              cancelButton: 'hidden'
+            }
+          });
+      })
+    
   })
   .post((req, res, next) => {
     const { id } = req.params
-    const { email, password } = req.body
+    let { userDisplayStatus, emailDisplayStatus, passwordDisplayStatus } = req.body
 
-    // Validations
-    if (!email || !password) {
-      res.render('auth/signup', {
-        errorMessage: "All the fields must be filled."
-      })
-      return
+    console.log(req.body, emailDisplayStatus)
+
+    let changeButtonUname = 'hidden'
+    if (userDisplayStatus === 'hidden') {
+      changeButtonUname = ''
+    }
+    if (userDisplayStatus == undefined) {
+      userDisplayStatus = 'hidden'
+      changeButtonUname = ''
     }
 
-    // const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}/ // Checks that it have 7 chars or more.
+    let changeButtonEmail = 'hidden'
+    if (emailDisplayStatus === 'hidden') {
+      changeButtonEmail = ''
+    }
+    if (emailDisplayStatus == undefined) {
+      emailDisplayStatus = 'hidden'
+      changeButtonEmail = ''
+    }
 
-    // if (!regex.test(password)) {
-    //   res.status(500).render('auth/signup', {
-    //     errorMessage: 'The password must have at least 8 characters'
-    //   })
-    //   return
-    // }
+    let changeButtonPassword = 'hidden'
+    if (passwordDisplayStatus === 'hidden') {
+      changeButtonPassword = ''
+    }
+    if (passwordDisplayStatus == undefined) {
+      passwordDisplayStatus = 'hidden'
+      changeButtonPassword = ''
+    }
 
-    bcrypt
+    User.findById(id)
+      .then(theUser => {
+        res.render(
+          'settings/settings', 
+          { 
+            userInSession: req.session.currentUser,
+            userData: theUser,
+            userDisplay: {
+              content: userDisplayStatus,
+              changeButton: changeButtonUname,
+              cancelButton: userDisplayStatus
+            },
+            emailDisplay: {
+              content: emailDisplayStatus,
+              changeButton: changeButtonEmail,
+              cancelButton: emailDisplayStatus
+            },
+            passwordDisplay: {
+              content: passwordDisplayStatus,
+              changeButton: changeButtonPassword,
+              cancelButton: passwordDisplayStatus
+            }
+          });
+      })
+    
+  })
+
+router.post('/settings/:id/uname', (req, res, next) => {
+  const { id } = req.params
+  const { username } = req.body
+
+  // Validations
+  if (username === "") {
+    res.redirect(`/settings/${id}`)
+    return
+  }
+
+  User.findByIdAndUpdate(id, { username }, { new: true })
+    .then(() => res.redirect(`/settings/${id}`))
+})
+
+router.post('/settings/:id/email', (req, res, next) => {
+  const { id } = req.params
+  const { email } = req.body
+
+  // Validations
+  if (!email) {
+    res.redirect(`/settings/${id}`)
+    return
+  }
+
+  User.findByIdAndUpdate(id, { email }, { new: true })
+    .then(() => res.redirect(`/settings/${id}`))
+})
+
+router.post('/settings/:id/password', (req, res, next) => {
+  const { id } = req.params
+  const { password } = req.body
+
+  // Validations
+  if (!password) {
+    res.redirect(`/settings/${id}`)
+    return
+  }
+
+  // const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}/ // Checks that it have 7 chars or more.
+
+  // if (!regex.test(password)) {
+  //   res.redirect(`/settings/${id}`)
+  //   return
+  // }
+
+  bcrypt
       .hash(password, saltRounds)
       .then(hashedPassword => {
-        return User.findByIdAndUpdate(id, { email, hashedPassword }, { new: true })
+        return User.findByIdAndUpdate(id, { hashedPassword }, { new: true })
       })
       .then(userFromDB => {
-        console.log('user information updated succesfully')
-        res.redirect(`/main-table/${id}`)
+        res.redirect(`/settings/${id}`)
+        return
       })
-      .catch(err => console.log(err))
-
-  })
+      .catch(error => {
+        console.log(error)
+      })
+})
 
 module.exports = router;
