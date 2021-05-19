43ad721bfda5320faeaf43da74618ed2480789bc
albertopformoso <albertopformoso@gmail.com>
2021-05-18 20:03:43 -0500

    corrections

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -3,17 +3,18 @@ const router  = express.Router();
 const Note = require('../models/Note.model')
 
 /* GET home page */
-router.route('/main-table')
+router.route('/main-table/:id')
   .get((req, res, next) => {
+    const { id } = req.params
     Note.find()
     .then(notesFromDb => {
       console.log('notes:', notesFromDb)
-      res.render('notes/main-table',{ notes: notesFromDb}) 
+      res.render(`notes/main-table`,{ notes: notesFromDb, userInSession: req.session.currentUser, userId: id }) 
     })
     
   })
   .post((req, res, next) => {
-
+    const { id } = req.params
     const { status } = req.body
     let toDoBoolean = false
     let doTodayBoolean    = false
@@ -31,11 +32,11 @@ router.route('/main-table')
       doneBoolean = true
     }
     
-    console.log('req.body:', req.body.status)
+    // console.log('req.body:', req.body.status)
     Note.create({title: "new note", content: "you can start writing here", status, toDoBoolean, doTodayBoolean, inProgressBoolean, doneBoolean})
     .then(() => {
-      console.log('note created succesfully')
-      res.redirect('/main-table')
+      // console.log('note created succesfully')
+      res.redirect(`/main-table/${id}`)
     })
   })
 
@@ -48,7 +49,7 @@ router.route('/main-table')
     Note.findByIdAndDelete(id)
       .then(updatedNote => {
         console.log('delete:', updatedNote)
-        res.redirect('/main-table')
+        res.redirect(`/main-table/${id}`)
         
       })
       .catch(e => console.log('There was an error while deleting the drone'))
