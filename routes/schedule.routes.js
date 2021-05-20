a7d8f1469db85e8956c56f2df14c870c7d76d1c3
fereber1294 <freyesber@gmail.com>
2021-05-20 12:19:53 -0500

    updated routes for schedule

--- routes/schedule.routes.js
+++ routes/schedule.routes.js
@@ -2,31 +2,47 @@ const express = require('express')
 const router = express.Router()
 const Subjects = require('../models/Subjects.model')
 
-router.route('/subjects')
+router.route('/subjects/:id')
   .get((req,res,next) => {
+    const { id } = req.params
     Subjects.find()
     .then(subsFromDb => {
       console.log('The subjects are: ', subsFromDb)
-      res.render('../views/schedule/subjects.hbs', {subjects: subsFromDb})
+      res.render('../views/schedule/subjects.hbs', {subjects: subsFromDb, userInSession: req.session.currentUser, userId: id})
     })
   })
   .post((req,res,next) =>{
+    const { id } = req.params
     console.log('req.body : ', req.body)
     Subjects.create({subjectName: "Add new Subject", teacher: "Teacher name"})
     .then(() => {
       console.log('new subject added')
-      res.redirect('/subjects')
+      res.redirect(`/subjects/${id}`)
     })
   })
 
+//edit subjects
+router.route('/subjects/:subjectId/edit')
+  .post((req,res,next) =>{
+    const {subjId} = req.params
+    const {subjectName, teacher} = req.body
+    console.log('req user' , req.body)
+    Subjects.findByIdAndUpdate(subjId, {subjectName, teacher})
+    .then(updatedSubj => {
+      console.log('updated subj: ', updatedSubj)
+      res.redirect(`/main-table/${subjId}`)
+    })
+  })
+
+
 //delete subjects
 
-  router.post('/subjects/:id/delete',(req,res,next) => {
+  router.post(`/subjects/:id/delete`,(req,res,next) => {
     const {id} = req.params
     Subjects.findByIdAndDelete(id)
       .then(updatedSubj => {
         console.log(updatedSubj, 'Deleted')
-        res.redirect('/subjects')
+        res.redirect(`/subjects/${id}`)
       })
       .catch(e => console.log('There was an error deleting the subject'))
   })
